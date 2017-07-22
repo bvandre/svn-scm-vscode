03b31249cf7068fc8058e0b4ea468ae8d75e6610
Brian Vandre <bvandre@gmail.com>
2017-07-22 10:11:02 -0500

    fill out scmProvider

--- src/scmProvider.ts
+++ src/scmProvider.ts
@@ -1,11 +1,49 @@
 "use_strict";
 
-import { scm, SourceControl } from "vscode";
+import {
+	Disposable,
+	QuickDiffProvider,
+	scm,
+	SourceControl,
+	SourceControlResourceGroup,
+	Uri
+} from "vscode";
 
-export class ScmProvider {
-	private mScm: SourceControl;
+export class ScmProvider implements QuickDiffProvider, Disposable {
+	private mDisposables: Disposable[] = [];
+	private mSourceControl: SourceControl;
+	private mChangesGroup: SourceControlResourceGroup;
+	private mStagedGroup: SourceControlResourceGroup;
 
 	constructor() {
-		this.mScm = scm.createSourceControl("svn", "Svn");
+		this.mSourceControl = scm.createSourceControl("svn", "Svn");
+		this.mSourceControl.acceptInputCommand = {
+			command: "svn.commitWithInput",
+			title: "Commit"
+		};
+		this.mSourceControl.quickDiffProvider = this;
+		this.mDisposables.push(this.mSourceControl);
+
+		this.mChangesGroup = this.mSourceControl.createResourceGroup("changes", "Changes");
+		this.mChangesGroup.hideWhenEmpty = true;
+		this.mDisposables.push(this.mChangesGroup);
+
+		this.mStagedGroup = this.mSourceControl.createResourceGroup("staged", "Staged Changes");
+		this.mStagedGroup.hideWhenEmpty = true;
+		this.mDisposables.push(this.mStagedGroup);
+	}
+
+	public provideOriginalResource(uri: Uri): Uri | undefined {
+		if (uri.scheme !== "file") {
+			return;
+		}
+
+		return uri.with({
+			scheme: "svn"
+		});
+	}
+
+	public dispose(): void {
+		this.mDisposables.forEach(d => d.dispose());
 	}
 }
