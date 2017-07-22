e4fd73858c7a19fa92b9625c047416278b2ee9af
Brian Vandre <bvandre@gmail.com>
2017-07-22 08:35:32 -0500

    add initial ScmProvider

---
+++ src/scmProvider.ts
@@ -0,0 +1,11 @@
+"use_strict";
+
+import { scm, SourceControl } from "vscode";
+
+export class ScmProvider {
+    private mScm: SourceControl;
+
+    constructor() {
+        this.mScm = scm.createSourceControl("svn", "Svn");
+    }
+}
